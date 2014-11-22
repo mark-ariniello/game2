2158327b54f0c5c19bf55a3b2ed19e376584cf7a
mark-ariniello <mark.ariniello@colorado.edu>
2014-11-21 17:29:41 -0700

    next commit
    updated some stuff

---
+++ Assets/MyAssets/Scripts/DoorOpen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpen : MonoBehaviour {
+	
+	private bool open = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (!open) {
+
+
+		}
+	}
+}
