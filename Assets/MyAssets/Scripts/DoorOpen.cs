d5007096abf6f9729a6f78ba7cd77fdca422ca8d
shatterspike1 <stupidinternet303@gmail.com>
2014-11-26 21:15:38 -0700

    Door Script, Camera Clipping Plane
    Also added an okayish looking clone model, does that work for you?

--- Assets/MyAssets/Scripts/DoorOpen.cs
+++ Assets/MyAssets/Scripts/DoorOpen.cs
@@ -4,20 +4,34 @@ using System.Collections;
 public class DoorOpen : MonoBehaviour {
 	
 	private bool open = false;
+	private int downtick = 0;
+	private Vector3 startPos;
+	public float dir = 1.9f;
 	// Use this for initialization
 	void Start () {
-	
+		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(open){
+			Vector3 hold = startPos;
+			hold.x = (hold.x + dir);
+			transform.position = Vector3.MoveTowards(transform.position, hold, Time.deltaTime);
+		}
+		if(!open){
+			transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime);
+		}
+		if(downtick > 0){
+		    downtick = downtick-1;
+		}
 	}
 
-	void OnTriggerStay (Collider other) {
-		if (!open) {
-
-
+	void Flip (){
+		if(downtick == 0){
+		    open = !open;
+		    Debug.Log ("Detected Door Open/Close Command.");
+		    downtick = 3;
 		}
 	}
 }
