90f6d4758771c641a210d6f9cf3133959f596699
mark-ariniello <mark.ariniello@colorado.edu>
2014-12-05 17:50:53 -0700

    more elevator stuff
    doors!

---
+++ Assets/MyAssets/Scripts/elevatortop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public GameObject topDoor;
+	public GameObject bottomDoor;
+	private bool close;
+	private Vector3 a;
+	private Vector3 b; 
+	private Vector3 c;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider other){
+		a = topDoor.transform.localPosition;
+		b = bottomDoor.transform.localPosition;
+		if (b[1] >= -2f) {
+	}
+}
